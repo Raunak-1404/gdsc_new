2cda9057546645824ba3f1cb4f62a50f991cfac7
Raunak Jijotia <140972655+Raunak-1404@users.noreply.github.com>
2023-09-09 22:49:00 +0530

    first commit and hopefully final

---
+++ main.js
@@ -0,0 +1,66 @@
+var all = document.querySelector("html");
+var flag = 0;
+var toggle_plane = false;
+var heading = document.querySelector(".RCOEM");
+var plane = document.querySelector(".plane");
+var sun = document.querySelector(".sun");
+var circle = document.querySelector(".circle");
+circle.addEventListener("click", () => {
+    sun.textContent = " ";
+    if(flag == 0) {
+        all.style.backgroundColor = "black";
+        heading.style.color = "white";
+        flag = 1;
+        
+        moveRight();
+    } 
+
+    else {
+        heading.style.color = "#646464";
+        all.style.backgroundColor = "white";
+        flag = 0;
+        moveLeft();
+    }
+})
+
+var id = null;
+var move = document.querySelector(".move");
+
+function moveRight() {
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 1);
+    function frame() {
+        if(pos == 55) {
+            clearInterval(id)
+        } else {
+            pos++;
+
+            move.style.left = pos + 'px';
+        }
+    }
+}
+
+
+function moveLeft () {
+    var pos = 55;
+    clearInterval(id);
+    id = setInterval(frame, 1);
+    function frame () {
+        if(pos == 10) {
+            clearInterval(id);
+        } else {
+            pos --;
+            move.style.left = pos + 'px';
+        }
+    }
+}
+
+
+var red = document.querySelector(".red-arrow");
+red.addEventListener("click", write);
+
+function write () {
+    let div = document.createElement("div");
+    
+}
